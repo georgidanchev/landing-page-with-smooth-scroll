423a18cd4b4449f4a7b3ea35c034dde3f07afe5e
w3althambition <georgicodes@outlook.com>
2019-05-05 15:30:16 +0100

    local eslint file removed. js file eslinted. fixed npm pug.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import './styles/style.scss'
-import OverlayScrollbars from 'overlayscrollbars'
+import './styles/style.scss';
+import OverlayScrollbars from 'overlayscrollbars';
 
 // extansiate the overlayscrollbars package
 const scrollInstance = OverlayScrollbars(document.body, {
-  className: 'os-theme-light',
-})
+  className: 'os-theme-light'
+});
 
 document.body.addEventListener('click', (e) => {
   // Prevent default behaviour of links.
-  e.preventDefault()
-  
+  e.preventDefault();
+
   // If the target is on of our links.
   if (e.target.classList.contains('navbar__link')) {
     // Scroll to that part of the page within 1s.
-   scrollInstance.scroll(document.querySelector(e.target.getAttribute('href')), 1000)
+    scrollInstance.scroll(document.querySelector(e.target.getAttribute('href')), 1000);
   }
-})
+});
